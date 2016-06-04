139d3b35f485f7e72afcb305e5d47b8e83627ef6
blacky99 <jaszczykwojciech@gmail.com>
2016-06-05 01:33:17 +0200

    myProjects - tworzenie buttonow

---
+++ src/projekt/MyProjects.java
@@ -0,0 +1,70 @@
+package projekt;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import baza.LoggedUser;
+
+public class MyProjects implements ActionListener{
+
+	protected ArrayList<CProject> myProjectsArray = new ArrayList<>();
+	protected JButton buttonTemp;
+	
+	protected ArrayList<JButton> buttonsArray = new ArrayList<>();
+	
+	private JPanel jPanel;
+	
+	
+	public MyProjects()
+	{
+		myProjectsArray = LoggedUser.getInstance().getUserProjects();
+		
+		for(int i = 0; i < myProjectsArray.size(); i++)
+		{
+			switch(i){
+				case 0:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 90, 70, 142, 139); }
+				case 1:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 330, 70, 142, 139); }
+				case 2:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 570, 70, 142, 139); }
+				case 3:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 90, 270, 142, 139); }
+				case 4:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 330, 270, 142, 139); }
+				case 5:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 570, 270, 142, 139); }
+				case 6:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 90, 470, 142, 139); }
+				case 7:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 330, 470, 142, 139); }
+				case 8:
+				{ addButton(LoggedUser.getInstance().getUserProjects().get(i).projectName, 570, 470, 142, 139); }	
+			}
+			
+		}
+		
+	}
+	
+	void addButton(String s, int x, int y, int width, int height)
+	{
+		buttonTemp = new JButton(s);
+		buttonTemp.setBounds(x, y, width, height);
+		buttonTemp.addActionListener(this);
+		jPanel.add(buttonTemp);
+		buttonsArray.add(buttonTemp);		
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	
+		
+}
